969bcafc07700fb0704aff17206ac85c60e8a47d
hzj495 <hzj694444990@gmail.com>
2024-12-12 13:03:01 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '107.175.54.19';
+let proxyIP = '31.186.85.171';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
