40228ecd24c0028088ff6eb9e7e2281927060667
hzj495 <hzj694444990@gmail.com>
2025-08-12 16:14:43 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = 'cf.nfjunka.xyz';
+let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
@@ -30,7 +30,7 @@ let addressesnotlsapi = [];
 let addressescsv = [];
 let DLS = 8;
 let remarkIndex = 1;//CSV备注所在列偏移量
-let FileName = '君卡';
+let FileName = atob('ZWRnZXR1bm5lbA==');
 let BotToken;
 let ChatID;
 let proxyhosts = [];
@@ -4566,6 +4566,3 @@ async function nginx() {
 	`
     return text;
 }
-
-
-
