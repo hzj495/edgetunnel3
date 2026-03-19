88c8c44c7acdb7f20e172482291a9000fa9f35f7
hzj495 <hzj694444990@gmail.com>
2026-03-19 16:04:35 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -3650,6 +3650,7 @@ function config_Html(token = "test", proxyhost = "") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script id="chatway" async="true" src="https://cdn.chatway.app/widget.js?id=S8pIVRBZNeF6"></script>
     <title id="pageTitle">配置页面</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
